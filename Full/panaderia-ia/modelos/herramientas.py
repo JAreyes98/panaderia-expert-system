e3d870e695ad9c3caea7e3e96d76e1cae13905b0
= <=>
2020-02-13 20:27:57 -0600

    Proyecto IA finalizado. Se elimaron archivos duplicados de la carpeta full

--- Full/panaderia-ia/modelos/herramientas.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Herramientas:
-    def __init__(self):
-        pass
-
-    def getDireccionIcono(self):
-        direccion = os.path.dirname(os.path.realpath(__file__)).replace('modelos','controladores/')
-        return os.path.join(direccion, 'recursos', 'icons8-bot-100.ico')
